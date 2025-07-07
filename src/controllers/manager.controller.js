3ecc538e2b5d383b27283a2121f048e887b859ac
Deymer Ospina <deymer.ospina@gmail.com>
2025-07-07 06:34:14 -0500

    Add project idea and freelance management endpoints
    Implemented new controller functions and routes for managing project ideas, freelance projects, and active projects. Added endpoints to create, retrieve, and activate project ideas and freelance projects, as well as to fetch collaborators with their IDs. Updated routes to expose these new functionalities.

--- src/controllers/manager.controller.js
+++ src/controllers/manager.controller.js
@@ -1,7 +1,8 @@
 import getConnection from "../database/connection.mysql.js"
 import { variablesDB } from "../utils/params/const.database.js";
+import { responseQueries } from "../common/enum/queries/response.queries.js";
 
-// Obtener datos
+// Obtener tipos de proyectos
 export const getDataProjectTypes = async (req, res) => {
   const conn = await getConnection();
   const db = variablesDB.data_base;
@@ -15,11 +16,126 @@ export const getDataProjectTypes = async (req, res) => {
   return res.json(select[0]);
 }
 
+// Obtener ideas de proyectos
+export const getDataProjectIdea = async (req, res) => {
+  const conn = await getConnection();
+  const db = variablesDB.data_base;
+  const query = `
+    SELECT id, title, description FROM ${db}.projects WHERE activate = 0`;
+  const select = await conn.query(query);
+  if (!select) return res.json({
+    status: 500,
+    message: 'Error obteniendo los datos'
+  });
+  return res.json(select[0]);
+}
+
+// Guardar idea
+export const saveProjectIdea = async (req, res) => {
+  const { title, description, type_id } = req.body;
+
+  if (!title || !description || !type_id) {
+    return res.json(responseQueries.error({ message: "Datos incompletos" }));
+  }
+
+  const conn = await getConnection();
+  const db = variablesDB.data_base;
+
+  const insert = await conn.query(
+    `INSERT INTO ${db}.projects (title, description, type_id) VALUES (?, ?, ?)`,
+    [title, description, type_id]
+  );
+
+  if (!insert) return res.json(responseQueries.error({ message: "Error al crear la idea" }));
+
+  return res.json(responseQueries.success({ message: "Idea creada con éxito" }));
+};
+
+// Obtener proyectos freelance
+export const getDataProjectFreelance = async (req, res) => {
+  const conn = await getConnection();
+  const db = variablesDB.data_base;
+  const query = `
+    SELECT id, title, description, progress, status FROM ${db}.projects WHERE type_id = 2`;
+  const select = await conn.query(query);
+  if (!select) return res.json({
+    status: 500,
+    message: 'Error obteniendo los datos'
+  });
+  return res.json(select[0]);
+}
+
+// Guardar freelance
+export const saveProjectFreelance = async (req, res) => {
+  const { title, description, type_id, status, activate } = req.body;
+
+  if (!title || !description || !type_id || !status || !activate) {
+    return res.json(responseQueries.error({ message: "Datos incompletos" }));
+  }
+
+  const conn = await getConnection();
+  const db = variablesDB.data_base;
+
+  const insert = await conn.query(
+    `INSERT INTO ${db}.projects (title, description, type_id, status, activate) VALUES (?, ?, ?, ?, ?)`,
+    [title, description, type_id, status, activate]
+  );
+
+  if (!insert) return res.json(responseQueries.error({ message: "Error al crear el proyecto freelance" }));
+
+  return res.json(responseQueries.success({ message: "Proyecto freelance creado con éxito" }));
+};
+
+// Obtener proyectos activos
+export const getDataProjectActive = async (req, res) => {
+  const conn = await getConnection();
+  const db = variablesDB.data_base;
+  const query = `
+    SELECT id, title, description, progress, status FROM ${db}.projects WHERE activate = 1`;
+  const select = await conn.query(query);
+  if (!select) return res.json({
+    status: 500,
+    message: 'Error obteniendo los datos'
+  });
+  return res.json(select[0]);
+}
+
+// Activar los proyectos
+
+export const updateActive = async (req, res) => {
+  // const { id } = req.params;
+  // const data = JSON.parse(req.body.data);
+  const { id } = req.body;
+
+  if (!id) {
+    return res.json(responseQueries.error({ message: "Datos incompletos" }));
+  }
+
+  try {
+    const conn = await getConnection();
+    const db = variablesDB.data_base;
+
+    const update = await conn.query(
+      `UPDATE ${db}.projects SET status = "starting", activate = 1 WHERE id = ?;`,
+      [id]
+    );
+
+    if (update.affectedRows === 0) {
+      return res.json(responseQueries.error({ message: "No se encontró la idea" }));
+    }
+
+    return res.json(responseQueries.success({ message: "Idea activada con éxito" }));
+  } catch (error) {
+    return res.json(responseQueries.error({ message: "Error al activar la idea", error }));
+  }
+};
+
+// Obtener colaboradores
 export const getDataCollaborators = async (req, res) => {
   const conn = await getConnection();
   const db = variablesDB.data_base;
   const query = `
-    SELECT name, department FROM ${db}.users`;
+    SELECT id, name, department FROM ${db}.users`;
   const select = await conn.query(query);
   if (!select) return res.json({
     status: 500,
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,7 @@ import { AuthorizationVerify } from '../middlewares/authorization.js';
 import { ConexionVerify } from '../middlewares/connection.js';
 
 // Controladores
-import { getDataProjectTypes, getDataCollaborators } from '../controllers/manager.controller.js';
+import { getDataProjectTypes, getDataCollaborators, getDataProjectIdea, getDataProjectFreelance, saveProjectIdea, saveProjectFreelance, getDataProjectActive, updateActive } from '../controllers/manager.controller.js';
 
 // Database
 import { getConnect } from '../database/conection.controller.js';
@@ -17,6 +17,12 @@ export const routes = () => {
 
     router.get('/manager/g/project-types', AuthorizationVerify, getDataProjectTypes);
     router.get('/manager/g/collaborators', AuthorizationVerify, getDataCollaborators);
+    router.get('/manager/g/project-idea', AuthorizationVerify, getDataProjectIdea);
+    router.get('/manager/g/project-freelance', AuthorizationVerify, getDataProjectFreelance);
+    router.get('/manager/g/project-active', AuthorizationVerify, getDataProjectActive);
+    router.post('/manager/i/add-project-idea', AuthorizationVerify, saveProjectIdea);
+    router.post('/manager/i/add-project-freelance', AuthorizationVerify, saveProjectFreelance);
+    router.put('/manager/u/active-idea', AuthorizationVerify, updateActive);
 
     // Database
     router.get('/conect/', ConexionVerify, getConnect);
